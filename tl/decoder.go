2ad748f1583c688b7a03795982188b7972a7b6af
Denis Subbotin <denis@subbot.in>
2022-05-25 22:49:31 +0300

    test and styles

--- tl/decoder.go
+++ tl/decoder.go
@@ -20,54 +20,68 @@ func decode(buf *bytes.Reader, val reflect.Value) error {
 	if !val.CanSet() {
 		return fmt.Errorf("value can't be changed")
 	}
+
 	switch val.Kind() {
 	case reflect.Uint32, reflect.Int32:
 		b := make([]byte, 4)
+
 		_, err := io.ReadFull(buf, b)
 		if err != nil {
 			return err
 		}
+
 		if val.Kind() == reflect.Uint32 {
 			val.SetUint(uint64(binary.LittleEndian.Uint32(b)))
 		} else {
 			val.SetInt(int64(binary.LittleEndian.Uint32(b)))
 		}
 		return nil
+
 	case reflect.Uint64, reflect.Int64:
 		b := make([]byte, 8)
+
 		_, err := io.ReadFull(buf, b)
 		if err != nil {
 			return err
 		}
+
 		if val.Kind() == reflect.Uint64 {
 			val.SetUint(binary.LittleEndian.Uint64(b))
 		} else {
 			val.SetInt(int64(binary.LittleEndian.Uint64(b)))
 		}
 		return nil
+
 	case reflect.Slice:
 		if val.Type().Elem().Kind() != reflect.Uint8 {
 			return fmt.Errorf("decoding slice of %v not supported", val.Type().Elem().Kind())
 		}
+
 		data, err := readByteSlice(buf)
 		if err != nil {
 			return err
 		}
+
 		val.SetBytes(data)
 		return nil
+
 	case reflect.Array:
 		if val.Type().Elem().Kind() != reflect.Uint8 {
 			return fmt.Errorf("decoding array of %v not supported", val.Type().Elem().Kind())
 		}
+
 		data, err := readByteSlice(buf)
 		if err != nil {
 			return err
 		}
+
 		if val.Type().Len() != len(data) {
 			return fmt.Errorf("mismatched lenghth of decoded byte slice (%v) and array (%v)", len(data), val.Type().Len())
 		}
+
 		reflect.Copy(val, reflect.ValueOf(data))
 		return nil
+
 	case reflect.Struct:
 		for i := 0; i < val.NumField(); i++ {
 			if !val.Field(i).CanSet() {
@@ -89,35 +103,47 @@ func readByteSlice(buf *bytes.Reader) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	var data []byte
 	var full int
-	if firstByte < 254 {
+
+	if firstByte < 0xFE {
 		data = make([]byte, int(firstByte))
+
 		_, err := io.ReadFull(buf, data)
 		if err != nil {
 			return nil, err
 		}
+
 		full = 1 + len(data)
-	} else if firstByte == 254 {
+
+	} else if firstByte == 0xFE {
 		sizeBuf := make([]byte, 4)
+
 		_, err := io.ReadFull(buf, sizeBuf[:3])
 		if err != nil {
 			return nil, err
 		}
+
 		data = make([]byte, binary.LittleEndian.Uint32(sizeBuf))
+
 		_, err = io.ReadFull(buf, data)
 		if err != nil {
 			return nil, err
 		}
+
 		full = 4 + len(data)
+
 	} else {
 		return nil, fmt.Errorf("invalid bytes prefix")
 	}
+
 	for ; full%4 != 0; full++ {
 		_, err = buf.ReadByte()
 		if err != nil {
 			return nil, err
 		}
 	}
+
 	return data, nil
 }
--- tl/encoder.go
+++ tl/encoder.go
@@ -14,51 +14,66 @@ func Marshal(o any) ([]byte, error) {
 	if m, ok := o.(MarshalerTL); ok {
 		return m.MarshalTL()
 	}
+
 	val := reflect.ValueOf(o)
+
 	switch val.Kind() {
 	case reflect.Uint32:
 		b := make([]byte, 4)
 		binary.LittleEndian.PutUint32(b, uint32(val.Uint()))
 		return b, nil
+
 	case reflect.Int32:
 		b := make([]byte, 4)
 		binary.LittleEndian.PutUint32(b, uint32(int32(val.Int())))
 		return b, nil
+
 	case reflect.Int64:
 		b := make([]byte, 8)
 		binary.LittleEndian.PutUint64(b, uint64(val.Int()))
 		return b, nil
+
 	case reflect.Uint64:
 		b := make([]byte, 8)
 		binary.LittleEndian.PutUint64(b, val.Uint())
 		return b, nil
+
 	case reflect.Slice:
 		if val.Type().Elem().Kind() != reflect.Uint8 {
 			return nil, fmt.Errorf("encoding slice of %v not supported", val.Type().Elem().Kind())
 		}
+
 		data := val.Bytes()
 		b := append(EncodeLength(len(data)), data...)
 		return zeroPadding(b), nil
+
 	case reflect.Array:
 		if val.Type().Elem().Kind() != reflect.Uint8 {
 			return nil, fmt.Errorf("encoding array of %v not supported", val.Type().Elem().Kind())
 		}
+
 		b := make([]byte, 0, val.Len()+8)
 		b = append(b, EncodeLength(val.Len())...)
+
 		for i := 0; i < val.Len(); i++ {
 			b = append(b, uint8(val.Index(i).Uint()))
 		}
 		return zeroPadding(b), nil
+
 	case reflect.Struct:
 		var buf []byte
+
 		for i := 0; i < val.NumField(); i++ {
 			b, err := Marshal(val.Field(i).Interface())
 			if err != nil {
 				return nil, err
 			}
+
 			buf = append(buf, b...)
 		}
+
 		return buf, nil
+
 	default:
 		return nil, fmt.Errorf("type %v not emplemented", val.Kind())
 	}
@@ -73,10 +88,10 @@ func zeroPadding(b []byte) []byte {
 }
 
 func EncodeLength(i int) []byte {
-	if i >= 254 {
+	if i >= 0xFE {
 		b := make([]byte, 4)
 		binary.LittleEndian.PutUint32(b, uint32(i<<8))
-		b[0] = 254
+		b[0] = 0xFE
 		return b
 	} else {
 		return []byte{byte(i)}
