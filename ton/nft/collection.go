8f41398cdc200b67fb42f0595afe52a56ff2f002
iam047801 <datbooi@tuta.io>
2022-09-07 22:03:47 +0300

    nft.collection: parse contract data, parse item single mint payload

--- ton/nft/collection.go
+++ ton/nft/collection.go
@@ -12,8 +12,17 @@ import (
 )
 
 type TonApi interface {
+	GetAccount(ctx context.Context, block *tlb.BlockInfo, addr *address.Address) (*tlb.Account, error)
 	CurrentMasterchainInfo(ctx context.Context) (_ *tlb.BlockInfo, err error)
 	RunGetMethod(ctx context.Context, blockInfo *tlb.BlockInfo, addr *address.Address, method string, params ...any) ([]interface{}, error)
+}
+
+type CollectionRawData struct {
+	OwnerAddress  *address.Address `tlb:"addr"`
+	NextItemIndex *big.Int         `tlb:"## 64"`
+	Content       *cell.Cell       `tlb:"^"`
+	ItemCode      *cell.Cell       `tlb:"^"`
+	RoyaltyParams *cell.Cell       `tlb:"^"`
 }
 
 type ItemMintPayload struct {
@@ -37,9 +46,9 @@ type CollectionData struct {
 }
 
 type CollectionRoyaltyParams struct {
-	Factor  uint16
-	Base    uint16
-	Address *address.Address
+	Factor  uint16           `tlb:"## 16"`
+	Base    uint16           `tlb:"## 16"`
+	Address *address.Address `tlb:"addr"`
 }
 
 type CollectionClient struct {
@@ -54,12 +63,41 @@ func NewCollectionClient(api TonApi, collectionAddr *address.Address) *Collectio
 	}
 }
 
+func (c *CollectionClient) ParseAccountData(ctx context.Context) (*CollectionRawData, error) {
+	b, err := c.api.CurrentMasterchainInfo(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get masterchain info: %w", err)
+	}
+	return c.ParseAccountDataAtBlock(ctx, b)
+}
+
+func (c *CollectionClient) ParseAccountDataAtBlock(ctx context.Context, block *tlb.BlockInfo) (*CollectionRawData, error) {
+	raw := new(CollectionRawData)
+
+	a, err := c.api.GetAccount(ctx, block, c.addr)
+	if err != nil {
+		return nil, err
+	}
+	if !a.IsActive || a.State.Status != tlb.AccountStatusActive {
+		return nil, fmt.Errorf("account is not active")
+	}
+
+	if err := tlb.LoadFromCell(raw, a.Data.BeginParse()); err != nil {
+		return nil, fmt.Errorf("load from cell: %w", err)
+	}
+
+	return raw, nil
+}
+
 func (c *CollectionClient) GetNFTAddressByIndex(ctx context.Context, index *big.Int) (*address.Address, error) {
 	b, err := c.api.CurrentMasterchainInfo(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get masterchain info: %w", err)
 	}
+	return c.GetNFTAddressByIndexAtBlock(ctx, b, index)
+}
 
+func (c *CollectionClient) GetNFTAddressByIndexAtBlock(ctx context.Context, b *tlb.BlockInfo, index *big.Int) (*address.Address, error) {
 	res, err := c.api.RunGetMethod(ctx, b, c.addr, "get_nft_address_by_index", index)
 	if err != nil {
 		return nil, fmt.Errorf("failed to run get_nft_address_by_index method: %w", err)
@@ -83,7 +121,10 @@ func (c *CollectionClient) RoyaltyParams(ctx context.Context) (*CollectionRoyalt
 	if err != nil {
 		return nil, fmt.Errorf("failed to get masterchain info: %w", err)
 	}
+	return c.RoyaltyParamsAtBlock(ctx, b)
+}
 
+func (c *CollectionClient) RoyaltyParamsAtBlock(ctx context.Context, b *tlb.BlockInfo) (*CollectionRoyaltyParams, error) {
 	res, err := c.api.RunGetMethod(ctx, b, c.addr, "royalty_params")
 	if err != nil {
 		return nil, fmt.Errorf("failed to run royalty_params method: %w", err)
@@ -121,7 +162,10 @@ func (c *CollectionClient) GetNFTContent(ctx context.Context, index *big.Int, in
 	if err != nil {
 		return nil, fmt.Errorf("failed to get masterchain info: %w", err)
 	}
+	return c.GetNFTContentAtBlock(ctx, b, index, individualNFTContent)
+}
 
+func (c *CollectionClient) GetNFTContentAtBlock(ctx context.Context, b *tlb.BlockInfo, index *big.Int, individualNFTContent *cell.Cell) (ContentAny, error) {
 	res, err := c.api.RunGetMethod(ctx, b, c.addr, "get_nft_content", index, individualNFTContent)
 	if err != nil {
 		return nil, fmt.Errorf("failed to run get_nft_content method: %w", err)
@@ -145,7 +189,10 @@ func (c *CollectionClient) GetCollectionData(ctx context.Context) (*CollectionDa
 	if err != nil {
 		return nil, fmt.Errorf("failed to get masterchain info: %w", err)
 	}
+	return c.GetCollectionDataAtBlock(ctx, b)
+}
 
+func (c *CollectionClient) GetCollectionDataAtBlock(ctx context.Context, b *tlb.BlockInfo) (*CollectionData, error) {
 	res, err := c.api.RunGetMethod(ctx, b, c.addr, "get_collection_data")
 	if err != nil {
 		return nil, fmt.Errorf("failed to run get_collection_data method: %w", err)
@@ -227,4 +274,40 @@ func (c *CollectionClient) BuildMintEditablePayload(index *big.Int, owner, edito
 	}
 
 	return body, nil
+}
+
+func ParseItemMintPayload(payload *cell.Cell) (*ItemMintPayload, error) {
+	ret := new(ItemMintPayload)
+
+	if err := tlb.LoadFromCell(ret, payload.BeginParse()); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+func ParseItemMintPayloadContent(payloadContent *cell.Cell) (owner, editor *address.Address, content ContentAny, err error) {
+	s := payloadContent.BeginParse()
+
+	owner, err = s.LoadAddr()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	contentSlice, err := s.LoadRef()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	content, err = ContentFromSlice(contentSlice)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	editor, err = s.LoadAddr()
+	if err != nil {
+		editor = nil
+	}
+
+	return owner, editor, content, nil
 }
