7a025faf7699cac5886853cbb0b4c22a58ab5c4d
SashaLawyer <alexbayb95@yandex.ru>
2022-12-14 11:07:29 +0300

    Tests: Added adnl tests

--- adnl/adnl_test.go
+++ adnl/adnl_test.go
@@ -3,7 +3,11 @@ package adnl
 import (
 	"context"
 	"crypto/ed25519"
+	"encoding/hex"
+	"errors"
+	"github.com/xssnick/tonutils-go/tl"
 	"testing"
+	"time"
 )
 
 func TestADNL_Connect(t *testing.T) {
@@ -18,11 +22,25 @@ func TestADNL_Connect(t *testing.T) {
 
 	err = adnl.Connect(context.Background(), "178.18.243.125:15888")
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
-	//var res any
-	//err = adnl.Query(context.Background(), &res)
-	//if err != nil {
-	//	t.Fatal()
-	//}
+
+	var res any
+	forRow, err := hex.DecodeString("e191b161")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = adnl.Query(ctx, tl.Raw(forRow), &res)
+	if err != nil {
+		if !errors.Is(err, ctx.Err()) {
+			t.Fatal(err.Error())
+		}
+	}
+	err = adnl.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
