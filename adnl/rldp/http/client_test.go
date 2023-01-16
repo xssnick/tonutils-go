08592e9df3355b9c06b2e927247cbdc046297670
SashaLawyer <alexbayb95@yandex.ru>
2023-01-16 10:42:11 +0300

    Tests: rldp/http: added alot tests

--- adnl/rldp/http/client_test.go
+++ adnl/rldp/http/client_test.go
@@ -3,14 +3,387 @@ package http
 import (
 	"bytes"
 	"context"
+	"crypto/ed25519"
+	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"github.com/xssnick/tonutils-go/adnl"
 	"github.com/xssnick/tonutils-go/adnl/dht"
+	"github.com/xssnick/tonutils-go/adnl/rldp"
 	"github.com/xssnick/tonutils-go/liteclient"
+	"github.com/xssnick/tonutils-go/tl"
 	"github.com/xssnick/tonutils-go/ton"
 	"github.com/xssnick/tonutils-go/ton/dns"
+	"io"
 	"net/http"
+	"reflect"
+	"sync"
 	"testing"
+	"time"
 )
+
+var originalConnector = Connector
+var originalNewRLDP = newRLDP
+
+type testRequest struct {
+	ID      []byte       `tl:"int256"`
+	Method  string       `tl:"string"`
+	URL     string       `tl:"string"`
+	Version string       `tl:"string"`
+	Headers []testHeader `tl:"vector struct"`
+}
+
+type testHeader struct {
+	Name  string `tl:"string"`
+	Value string `tl:"string"`
+}
+
+type testResponse struct {
+	Version    string       `tl:"string"`
+	StatusCode int32        `tl:"int"`
+	Reason     string       `tl:"string"`
+	Headers    []testHeader `tl:"vector struct"`
+	NoPayload  bool         `tl:"bool"`
+}
+
+type MockADNL struct {
+	query                   func(ctx context.Context, req, result tl.Serializable) error
+	setDisconnectHandler    func(handler func(addr string, key ed25519.PublicKey))
+	setCustomMessageHandler func(handler func(msg *adnl.MessageCustom) error)
+	sendCustomMessage       func(ctx context.Context, req tl.Serializable) error
+	close                   func()
+}
+
+func (m MockADNL) Query(ctx context.Context, req, result tl.Serializable) error {
+	return m.query(ctx, req, result)
+}
+
+func (m MockADNL) SetDisconnectHandler(handler func(addr string, key ed25519.PublicKey)) {
+}
+
+func (m MockADNL) SetCustomMessageHandler(handler func(msg *adnl.MessageCustom) error) {
+}
+
+func (m MockADNL) SendCustomMessage(ctx context.Context, req tl.Serializable) error {
+	return m.sendCustomMessage(ctx, req)
+}
+
+func (m MockADNL) Close() {}
+
+type MockRDLP struct {
+	close           func()
+	doQuery         func(ctx context.Context, maxAnswerSize int64, query, result tl.Serializable) error
+	setOnQuery      func(handler func(transferId []byte, query *rldp.Query) error)
+	setOnDisconnect func(handler func())
+	sendAnswer      func(ctx context.Context, maxAnswerSize int64, queryId, transferId []byte, answer tl.Serializable) error
+}
+
+func (m MockRDLP) Close() {}
+
+func (m MockRDLP) DoQuery(ctx context.Context, maxAnswerSize int64, query, result tl.Serializable) error {
+	return m.doQuery(ctx, maxAnswerSize, query, result)
+}
+
+func (m MockRDLP) SetOnQuery(handler func(transferId []byte, query *rldp.Query) error) {}
+
+func (m MockRDLP) SetOnDisconnect(handler func()) {}
+
+func (m MockRDLP) SendAnswer(ctx context.Context, maxAnswerSize int64, queryId, transferId []byte, answer tl.Serializable) error {
+	return m.sendAnswer(ctx, maxAnswerSize, queryId, transferId, answer)
+}
+
+//type MockDHT struct {
+//	storeAddress		func(ctx context.Context, addresses address.List, ttl time.Duration, ownerKey ed25519.PrivateKey, copies int) (int, []byte, error)
+//	findAddresses 		func(ctx context.Context, key []byte) (*address.List, ed25519.PublicKey, error)
+//	close				func()
+//}
+//
+//func (m MockADHT) StoreAddress(ctx context.Context, addresses address.List, ttl time.Duration, ownerKey ed25519.PrivateKey, copies int) (int, []byte, error){
+//	return 0, nil, nil
+//}
+//
+//func (m MockADHT) FindAddresses(ctx context.Context, key []byte) (*address.List, ed25519.PublicKey, error){
+//	return nil, nil, nil
+//}
+//
+//func (m MockADHT) Close(){}
+
+func TestTransport_removeRLDP(t *testing.T) {
+	var Connector = func(ctx context.Context, addr string, peerKey ed25519.PublicKey, ourKey ed25519.PrivateKey) (ADNL, error) {
+		return MockADNL{}, nil
+	}
+	pub, _, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal("failed to prepare test keys, err: ", err)
+	}
+	tAdnlConn, err := Connector(context.Background(), "1.2.3.4:12345", pub, nil)
+	if err != nil {
+		t.Fatal("failed to prepare test adnl connection, err: ", err)
+	}
+	var newRLDP = func(a ADNL) RLDP {
+		return rldp.NewClient(a)
+	}
+	tRldp := newRLDP(tAdnlConn)
+
+	tTransport := NewTransport(&MockDHT{}, MockResolver{})
+
+	tTransport.rldpInfos["123"] = &rldpInfo{
+		mx:             sync.RWMutex{},
+		ActiveClient:   tRldp,
+		ClientLastUsed: time.Time{},
+		ID:             nil,
+		Addr:           "",
+		Resolved:       false,
+	}
+
+	tTransport.removeRLDP(tRldp, "123")()
+	if tTransport.rldpInfos["123"].ActiveClient != nil {
+		t.Error("got active client after removeRLDP")
+	}
+}
+
+func TestTransport_getRLDPQueryHandler(t *testing.T) {
+	var Connector = func(ctx context.Context, addr string, peerKey ed25519.PublicKey, ourKey ed25519.PrivateKey) (ADNL, error) {
+		return MockADNL{}, nil
+	}
+	pub, _, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal("failed to prepare test keys, err: ", err)
+	}
+	tAdnlConn, err := Connector(context.Background(), "1.2.3.4:12345", pub, nil)
+	if err != nil {
+		t.Fatal("failed to prepare test adnl connection, err: ", err)
+	}
+
+	tQueryId := make([]byte, 32)
+	_, err = rand.Read(tQueryId)
+	if err != nil {
+		t.Fatal("failed to prepare test query id, err: ", err)
+	}
+
+	tQuery := &rldp.Query{
+		ID:            tQueryId,
+		MaxAnswerSize: int64(_RLDPMaxAnswerSize),
+		Timeout:       int32(123456),
+		Data: GetNextPayloadPart{
+			ID:           tQueryId,
+			Seqno:        0,
+			MaxChunkSize: _ChunkSize,
+		},
+	}
+
+	var newRLDP = func(a ADNL) RLDP {
+		return MockRDLP{
+			close:           nil,
+			doQuery:         nil,
+			setOnQuery:      nil,
+			setOnDisconnect: nil,
+			sendAnswer: func(ctx context.Context, maxAnswerSize int64, queryId, transferId []byte, answer tl.Serializable) error {
+				if !bytes.Equal(queryId, tQueryId) {
+					t.Fatal("received wrong query id")
+				}
+				return nil
+			},
+		}
+	}
+	tRldp := newRLDP(tAdnlConn)
+
+	t.Run("not last part", func(t *testing.T) {
+		tTransport := NewTransport(&MockDHT{}, MockResolver{})
+		dataStream := newDataStreamer()
+		dataStream.closed = false
+
+		tTransport.activeRequests[hex.EncodeToString(tQueryId)] = &payloadStream{
+			Data:      dataStream,
+			ValidTill: time.Now().Add(15 * time.Second),
+		}
+		go func() {
+			dataStream.parts <- []byte("123")
+		}()
+		go func() {
+			dataStream.parts <- nil
+		}()
+
+		err = tTransport.getRLDPQueryHandler(tRldp)(nil, tQuery)
+		if err != nil {
+			t.Fatal("failed to execute getRLDPQueryHandler func, err: ", err)
+		}
+		if _, ok := tTransport.activeRequests[hex.EncodeToString(tQueryId)]; !ok {
+			t.Error("active request is closed before last part sent")
+		}
+	})
+
+	t.Run("last part", func(t *testing.T) {
+		tTransport := NewTransport(&MockDHT{}, MockResolver{})
+		dataStream := newDataStreamer()
+		dataStream.closed = false
+
+		tTransport.activeRequests[hex.EncodeToString(tQueryId)] = &payloadStream{
+			Data:      dataStream,
+			ValidTill: time.Now().Add(15 * time.Second),
+		}
+		go func() {
+			dataStream.finished = true
+			dataStream.parts <- nil
+		}()
+		err = tTransport.getRLDPQueryHandler(tRldp)(nil, tQuery)
+		if err != nil {
+			t.Fatal("failed to execute getRLDPQueryHandler func, err: ", err)
+		}
+		if _, ok := tTransport.activeRequests[hex.EncodeToString(tQueryId)]; ok {
+			t.Error("find active request after last part sent")
+		}
+	})
+}
+
+func Test_handleGetPart(t *testing.T) {
+	tQueryId := make([]byte, 32)
+	_, err := rand.Read(tQueryId)
+	if err != nil {
+		t.Fatal("failed to prepare test query id, err: ", err)
+	}
+
+	tReqest := GetNextPayloadPart{
+		ID:           tQueryId,
+		Seqno:        0,
+		MaxChunkSize: _ChunkSize,
+	}
+
+	t.Run("not last part", func(t *testing.T) {
+		dataStream := newDataStreamer()
+		go func() {
+			dataStream.parts <- []byte{0xFF, 0xFF} // random bytes
+			dataStream.parts <- nil
+		}()
+
+		tTransport := NewTransport(&MockDHT{}, MockResolver{})
+		tTransport.activeRequests[hex.EncodeToString(tQueryId)] = &payloadStream{
+			Data:      dataStream,
+			ValidTill: time.Now().Add(15 * time.Second),
+		}
+		payLoadStr, _ := tTransport.activeRequests[hex.EncodeToString(tQueryId)]
+
+		payLoad, err := handleGetPart(tReqest, payLoadStr)
+		if err != nil {
+			t.Fatal("failed to execute handleGetPart func, err: ", err)
+		}
+
+		if !bytes.Equal(payLoad.Data, []byte{0xFF, 0xFF}) {
+			t.Error("got wrong bytes in pay load")
+		}
+		if payLoad.IsLast != false {
+			t.Error("got wrong payLoad.IsLast status")
+		}
+	})
+
+	t.Run("last part", func(t *testing.T) {
+		dataStream := newDataStreamer()
+		go func() {
+			dataStream.finished = true
+			dataStream.parts <- nil
+		}()
+
+		tTransport := NewTransport(&MockDHT{}, MockResolver{})
+		tTransport.activeRequests[hex.EncodeToString(tQueryId)] = &payloadStream{
+			Data:      dataStream,
+			ValidTill: time.Now().Add(15 * time.Second),
+		}
+		payLoadStr, _ := tTransport.activeRequests[hex.EncodeToString(tQueryId)]
+
+		payLoad, err := handleGetPart(tReqest, payLoadStr)
+		if err != nil {
+			t.Fatal("failed to execute handleGetPart func, err: ", err)
+		}
+		if len(payLoad.Data) != 0 {
+			t.Error("got wrong bytes in pay load")
+		}
+		if payLoad.IsLast != true {
+			t.Error("got wrong payLoad.IsLast status")
+		}
+	})
+}
+
+func TestTransport_RoundTripUnit(t *testing.T) {
+	Connector = func(ctx context.Context, addr string, peerKey ed25519.PublicKey, ourKey ed25519.PrivateKey) (ADNL, error) {
+		return MockADNL{
+				query: func(ctx context.Context, req, result tl.Serializable) error {
+					return nil
+				},
+				setDisconnectHandler: func(handler func(addr string, key ed25519.PublicKey)) {
+				},
+				setCustomMessageHandler: func(handler func(msg *adnl.MessageCustom) error) {
+				},
+				sendCustomMessage: func(ctx context.Context, req tl.Serializable) error {
+					fmt.Println(123)
+					return nil
+				},
+				close: func() {
+				},
+			},
+			nil
+	}
+
+	newRLDP = func(a ADNL) RLDP {
+		return &MockRDLP{
+			close: func() {
+			},
+			doQuery: func(ctx context.Context, maxAnswerSize int64, query, result tl.Serializable) error {
+				reflect.ValueOf(result).Elem().Set(reflect.ValueOf(Response{
+					Version:    "HTTP/1.1",
+					StatusCode: int32(200),
+					Reason:     "test ok",
+					Headers:    []Header{{"test", "test"}},
+					NoPayload:  true,
+				}))
+				return nil
+			},
+			setOnQuery: func(handler func(transferId []byte, query *rldp.Query) error) {
+			},
+			setOnDisconnect: func(handler func()) {
+			},
+			sendAnswer: func(ctx context.Context, maxAnswerSize int64, queryId, transferId []byte, answer tl.Serializable) error {
+				return nil
+			},
+		}
+	}
+	pub, _, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal("failed to prepare test keys, err: ", err)
+	}
+	tAdnlConn, err := Connector(context.Background(), "1.2.3.4:12345", pub, nil)
+	if err != nil {
+		t.Fatal("failed to prepare test adnl connection, err: ", err)
+	}
+
+	tRldpCli := newRLDP(tAdnlConn)
+	tTransport := NewTransport(&MockDHT{}, MockResolver{})
+
+	req, err := http.NewRequest(http.MethodGet, "http://foundation.ton/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.ContentLength = 1
+	req.Header["for test1"] = []string{"for test1.1", "for test1.2"}
+	req.Header["for test2"] = []string{"for test2.1", "for test2.2"}
+	req.Body = io.NopCloser(bytes.NewBufferString("body for testing"))
+
+	tTransport.rldpInfos[req.Host] = &rldpInfo{
+		mx:             sync.RWMutex{},
+		ActiveClient:   tRldpCli,
+		ClientLastUsed: time.Now().Add(-16 * time.Second),
+		ID:             nil,
+		Addr:           "",
+		Resolved:       true,
+	}
+	response, err := tTransport.RoundTrip(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Errorf("got response code '%d', want '200'", response.StatusCode)
+	}
+}
 
 func Test_parseADNLAddress(t *testing.T) {
 	res, err := ParseADNLAddress("ui52b4urpcoigi26kfwp7vt2cgs2b5ljudwigvra35nhvymdqvqlfsa")
@@ -23,7 +396,9 @@ func Test_parseADNLAddress(t *testing.T) {
 	}
 }
 
-func TestTransport_RoundTrip(t *testing.T) {
+func TestTransport_RoundTripIntegration(t *testing.T) {
+	Connector = originalConnector
+	newRLDP = originalNewRLDP
 	dhtClient, err := dht.NewClientFromConfigUrl(context.Background(), "https://ton-blockchain.github.io/global.config.json")
 	if err != nil {
 		t.Fatal(err)
--- adnl/rldp/http/server_test.go
+++ adnl/rldp/http/server_test.go
@@ -1,1 +1,81 @@
 package http
+
+import (
+	"bytes"
+	"context"
+	"crypto/ed25519"
+	"github.com/xssnick/tonutils-go/tl"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestServer_fetchPayload(t *testing.T) {
+	_, tPrivKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal("failed to prepare test private key, err: ", err)
+	}
+
+	mx := http.NewServeMux()
+	mx.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hop hey 777"))
+	})
+
+	tSrv := NewServer(tPrivKey, &MockDHT{}, mx)
+	w := newDataStreamer()
+	err = tSrv.fetchPayload(context.Background(), []byte("lol"), MockRDLP{
+		close: nil,
+		doQuery: func(ctx context.Context, maxAnswerSize int64, query, result tl.Serializable) error {
+			switch q := query.(type) {
+			case GetNextPayloadPart:
+				if !bytes.Equal(q.ID, []byte("lol")) {
+					t.Fatal("wrong id received in doQuery")
+				}
+				reflect.ValueOf(result).Elem().Set(reflect.ValueOf(PayloadPart{
+					Data:    []byte("test"),
+					Trailer: nil,
+					IsLast:  true,
+				}))
+			}
+			return nil
+		},
+		setOnQuery:      nil,
+		setOnDisconnect: nil,
+		sendAnswer:      nil,
+	}, w)
+	if err != nil {
+		t.Fatal("failed to execute ListenAndServe func")
+	}
+	time.Sleep(time.Second)
+	if !bytes.Equal(<-w.parts, []byte("test")) {
+		t.Error("invalid data in data stream parts")
+	}
+
+}
+
+func TestServer_Stop(t *testing.T) {
+	_, tPrivKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal("failed to prepare test private key, err: ", err)
+	}
+
+	mx := http.NewServeMux()
+	mx.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hop hey 777"))
+	})
+
+	tSrv := NewServer(tPrivKey, &MockDHT{}, mx)
+
+	err = tSrv.Stop()
+	if err != nil {
+		t.Fatal("failed to execute Stop func, err: ", err)
+	}
+	if tSrv.closed != true {
+		t.Error("closed flag == ture after Stop method")
+	}
+	if _, ok := <-tSrv.closer; ok != false {
+		t.Error("closer chan not closed after Stop method")
+
+	}
+}
